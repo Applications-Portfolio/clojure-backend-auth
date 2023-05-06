94b2090ff7601f972b789675cbc988c80817b416
Eduardo Lopes <eduardo.clopes@quintoandar.com.br>
2023-05-06 17:39:31 -0400

    Initial commit with template

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cloped/clojure-backend-auth
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cloped/clojure_backend_auth_test.clj
@@ -0,0 +1,7 @@
+(ns cloped.clojure-backend-auth-test
+  (:require [clojure.test :refer :all]
+            [cloped.clojure-backend-auth :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
